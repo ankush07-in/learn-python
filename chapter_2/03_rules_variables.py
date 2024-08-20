7daee38965cfc09ce5175260e621b5b7d0b1249e
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-20 21:29:35 +0530

    py-basics

---
+++ chapter_2/03_rules_variables.py
@@ -0,0 +1,19 @@
+# vaild variables
+
+a = 45;
+
+aaa = 56;
+
+ankush = 29;
+
+_ankush = 34;
+
+# invalid variables
+
+# @ankush = 39;
+
+# s gg = 89
+
+# 4nkush = 67;
+
+# s@meer = 90;
---
+++ chapter_2/05_type.py
@@ -0,0 +1,16 @@
+a = 45;
+t = type(a);
+print(t);
+
+b = 32.5;
+t = type(b);
+print(t);
+
+c = "Ankush";
+t = type(c);
+print(t);
+
+d = "31"
+e = float(d)
+t = type(d);
+print(e);
