3ec50d8aaaebd2d7e8c5a54a9ee7ff20f231d5d6
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-28 00:20:00 +0530

    update

--- chapter_11/06_property_decorators.py
+++ chapter_11/06_property_decorators.py
@@ -7,16 +7,17 @@ class Employee:
 
     @property
     def name(self):
-        return self.ename
+        return f"{self.fname} {self.lname}"
     
     @name.setter
     def name(self, value):
-        self.ename = value
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
 
 e = Employee()
 e.a = 45
 
-e.name = "Ankush"
+e.name = "Ankush Adhikari"
 print(e.name)
 
 e.show()
