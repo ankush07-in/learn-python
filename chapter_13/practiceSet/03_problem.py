d14dcc7cb3337e4312ec43633ec975db362622d2
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-29 02:18:39 +0530

    Solution 3
    A list contains the multiplication table of 7, write a program to convert it into vertical string of same numbers

---
+++ chapter_13/practiceSet/03_problem.py
@@ -0,0 +1,10 @@
+# A list contains the multiplication table of 7, write a program to convert it into vertical string of same numbers
+
+table = [str(7*i) for i in range(1, 11)]
+s = "\n".join(table)
+
+print(s)
+
+
+
+...
