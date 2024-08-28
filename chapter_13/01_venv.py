0e44680c1b3b4c094ad06500cf79eedadfa69993
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-29 00:04:20 +0530

    Virtual Environment
    An environment which is same as the system interpreter but is isolated from the other Python environments on the system

---
+++ chapter_13/01_venv.py
@@ -0,0 +1,20 @@
+'''
+
+# Virtual Environment
+
+An environment which is same as the system interpreter but is isolated from the other Python environments on the system
+
+
+# Installation 
+
+To use virtual environments, we write in terminal:
+
+    'pip install virtualenv' # => Install the package
+
+# To Activate run in terminal
+
+    './env/Scripts/activeate.ps1'
+    
+        
+
+'''
