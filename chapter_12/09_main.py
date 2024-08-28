641f6ff6f491f04d1ae902161811c9be542bb2f0
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-28 19:39:08 +0530

    __name__ == __main__

---
+++ chapter_12/09_main.py
@@ -0,0 +1,13 @@
+from module import myFunc
+
+
+'''
+** if __name__ == '__main__' in python **
+
+'__name__' evaluates to the name of the module in python from where the program is ran.
+
+If the the module is being run directly from the command line, the '__name__' is set to string '__main__' , Thus, this behaviour is used to check whether the module is run directly or imported to another file.
+
+
+
+'''
