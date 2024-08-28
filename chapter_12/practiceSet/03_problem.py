701e08a097f0114dd2fe8569f7e3d408fe1735a4
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-28 23:18:26 +0530

    Solution 3
    Write a list comprehension to print a list which contains the multiplication table of a user entered number

---
+++ chapter_12/practiceSet/03_problem.py
@@ -0,0 +1,10 @@
+# Write a list comprehension to print a list which contains the multiplication table of a user entered number
+
+n = int(input("Enter a number to print the table in list: "))
+
+table = [n*i for i in range(1, 11)]
+print(table)
+
+
+
+...
