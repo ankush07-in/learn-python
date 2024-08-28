7f2a54339d45e87d264f16e27fc9d95a2c063f58
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-28 22:44:03 +0530

    enumerate

---
+++ chapter_12/11_enumerate.py
@@ -0,0 +1,17 @@
+l = [3, 513, 53, 535]
+
+# index = 0
+# for i in l:
+#     print(f"The item number at index {index} is {i}")
+#     # index = index + 1
+#     index += 1
+
+
+
+# This can be simplified using enumerate function
+
+for index, i in enumerate(l):
+    print(f"The item number at index {index} is {i}")
+
+
+...
