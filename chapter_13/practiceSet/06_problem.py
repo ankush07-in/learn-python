db3a0e55686daf33825046ef7151ffd6a490d047
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-29 11:07:17 +0530

    Solution 6
    Run pip freeze for the system interpreter, Take the contents and create a similar virtualenv

---
+++ chapter_13/practiceSet/06_problem.py
@@ -0,0 +1,22 @@
+# Run pip freeze for the system interpreter, Take the contents and create a similar virtualenv
+
+
+'''
+
+in terminal we have to write these commands
+
+
+pip freeze > requirements.txt
+
+virtualenv {env_name}
+
+./{env_name}/Scripts/activate.ps1
+
+pip install -r requirements.txt
+
+
+
+'''
+
+
+...
