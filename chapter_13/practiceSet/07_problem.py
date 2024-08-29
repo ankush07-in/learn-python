22f679cd27678d3b6407d78fd9715022fe2405f6
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-29 11:16:02 +0530

    Solution 7
    Explore the 'Flask' module and create a web server using Flask & Python

---
+++ chapter_13/practiceSet/07_problem.py
@@ -0,0 +1,21 @@
+# Explore the 'Flask' module and create a web server using Flask & Python
+
+'''
+pip install flask
+
+flask basically use to create 'websites' or 'APIs'
+
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+app.run()
+
+
+...
